b3a8113f75a2fb2cf3534654cca22c490a4f0a86
yuzhiyizhan <liangrizhan18666@163.com>
2020-05-23 23:05:40 +0800

    添加容错

--- Commom/LOG.py
+++ Commom/LOG.py
@@ -1,18 +1,31 @@
+import time
 from loguru import logger
 
 
 def log(prints, Boolean=True):
     if Boolean == True:
-        logger.level("PASS", no=38, color="<green>", icon="🐍")
-        logger.log("PASS", str(prints))
+        try:
+            logger.level("PASS", no=38, color="<green>", icon="🐍")
+            logger.log("PASS", str(prints))
+        except:
+            print(f"\033[0;32;40m{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}\033[0m",
+                  "\033[0;32;40mPASS\033[0m", f"\033[0;32;40m{prints}\033[0m")
 
     elif Boolean == False:
-        logger.level("FALSE", no=38, color="<red>", icon="🐍")
-        logger.log("FALSE", str(prints))
+        try:
+            logger.level("FALSE", no=38, color="<red>", icon="🐍")
+            logger.log("FALSE", str(prints))
+        except:
+            print(f"\033[0;32;40m{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}\033[0m",
+                  "\033[0;31;40mFALSE\033[0m", f"\033[0;31;40m{prints}\033[0m")
 
     elif Boolean == "INFO":
-        logger.level("INFO", no=38, color="<blue>", icon="")
-        logger.log("INFO", str(prints))
+        try:
+            logger.level("INFO", no=38, color="<blue>", icon="")
+            logger.log("INFO", str(prints))
+        except:
+            print(f"\033[0;32;40m{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}\033[0m",
+                  "\033[0;34;40mINFO\033[0m", f"\033[0;34;40m{prints}\033[0m")
 
     else:
         raise "log函数没设置布尔值或INFO"
